da514f682e2ec2b3c48c27b74ca099339d1c215a
jnwcki <jude.nowicki@gmail.com>
2016-03-17 13:33:21 -0400

    index page with search bar

--- tab_scraper/urls.py
+++ tab_scraper/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from scraperapp.views import IndexView, TabView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^tabs/', TabView.as_view(), name='tabs')
 ]
